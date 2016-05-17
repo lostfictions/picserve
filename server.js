cb1a9df1fe3d7c59ee98b1d79694c6b56cb5244b
sa <sixsecondfiesta@gmail.com>
2016-05-16 22:04:40 -0400

    open images!

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const opn = require('opn')
 const express = require('express')
 const chokidar = require('chokidar')
 
+const fs = require('fs')
+
 const imagesFolder = path.join(process.env.HOME, 'Pictures')
 const imagePaths = []
 
@@ -25,6 +27,16 @@ app.use('/images', express.static(imagesFolder))
 app.get('/imagelist', (req, res) => {
   const imgs = _.shuffle(imagePaths)
   res.jsonp(imgs)
+})
+app.get('/open/:file', (req, res) => {
+  const p = path.join(imagesFolder, req.params.file)
+  if(fs.existsSync(p)) {
+    opn(p)
+    res.status(200).end()
+  }
+  else {
+    res.status(500).end()
+  }
 })
 
 app.listen(PORT)
